707a9555154af2966e592c1f595b0a81e29f1513
Stefano L <stefanosemail@protonmail.com>
2022-07-03 15:58:08 +0100

    workflow yml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import PlayerLayout from '../components/PlayerLayout'
 import Theme from '../components/Theme'
 import '../styles/global.css'
