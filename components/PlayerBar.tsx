c0e8547aad8533f89e0cb46cd4abfdce5d4ea133
Stefano L <stefanosemail@protonmail.com>
2022-07-04 04:00:08 +0100

    initial layout

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -3,11 +3,9 @@ import { Box, Text } from '@chakra-ui/layout'
 const PlayerBar = () => {
   return (
     <Box
-      height="100px"
-      width="100vw"
+      className="footer"
       bg="blue.500"
       color="white"
-      padding="10px"
     >
       <Text>Playerbar</Text>
     </Box>
--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,160 +1,139 @@
 import NextImage from 'next/image'
 import NextLink from 'next/link'
-import {
-  Box,
-  List,
-  ListItem,
-  LinkOverlay,
-  LinkBox,
-  ListIcon,
-  Divider,
-  Text,
-  Flex,
-} from '@chakra-ui/layout'
+import { Box, List, ListItem, LinkOverlay, LinkBox, ListIcon, Divider, Text, Flex } from '@chakra-ui/layout'
 import bart from '../../public/images/bart.png'
 import { navMenu, musicMenu } from './sideMenuRoutes'
 
 const playlists = new Array(10).fill(1).map((_, i) => {
-  return (
-    <Text
-      color="gray.500"
-      align="left"
-      paddingX="3rem"
-    >
-      {`Playlist ${i + 1}`}
-    </Text>
-  )
+   return (
+      <Text
+         key={i}
+         color="gray.500"
+         align="left"
+         paddingX="3rem">
+         {`Playlist ${i + 1}`}
+      </Text>
+   )
 })
 
 const Sidebar = () => {
-  return (
-    <Box
-      className="side-wrapper"
-      bg="gray.900"
-    >
-      <Box className="side_inner-wrapper">
-        <Box className="logo">
-          <Flex
-            justifyContent="flex-start"
-            paddingX="20px"
-            marginBottom="2rem"
-          >
-            <NextImage
-              src={bart}
-              height={36}
-              width={30}
-              alt="Bart"
-            />{' '}
-            <Text
-              ml={2}
-              textTransform="uppercase"
-              fontSize="sm"
-              marginLeft="0.5rem"
-              lineHeight="250%"
-              fontWeight="bold"
-              color="red.500"
-            >
-              TMX Media
-            </Text>
-          </Flex>
-        </Box>
-        <List spacing={2}>
-          {navMenu().map(menu => (
-            <ListItem
-              className="sidenav-links"
-              color="gray.600"
-              paddingX="20px"
-              fontSize="16px"
-              key={menu.name}
-            >
-              <LinkBox>
-                <NextLink
-                  href={menu.route}
-                  passHref
-                >
-                  <LinkOverlay>
-                    <ListIcon
-                      as={menu.icon}
-                      color="gray.500"
-                      marginRight="20px"
-                    />
-                    {menu.name}
-                  </LinkOverlay>
-                </NextLink>
-              </LinkBox>
-            </ListItem>
-          ))}
-        </List>
-        <Divider
-          className="side-divider"
-          color="gray.500"
-        />
-        <List spacing={2}>
-          {musicMenu().map(menu => (
-            <ListItem
-              paddingX="20px"
-              fontSize="16px"
-              className="sidenav-links"
-              color="gray.600"
-              key={menu.name}
-            >
-              <LinkBox>
-                <NextLink
-                  href={menu.route}
-                  passHref
-                >
-                  <LinkOverlay>
-                    <ListIcon
-                      as={menu.icon}
-                      color="gray.500"
-                      marginRight="20px"
-                    />
-                    {menu.name}
-                  </LinkOverlay>
-                </NextLink>
-              </LinkBox>
-            </ListItem>
-          ))}
-        </List>
-        <Divider
-          className="side-divider"
-          color="gray.500"
-        />
-        <List spacing={2}>
-          {musicMenu().map(menu => (
-            <ListItem
-              paddingX="20px"
-              fontSize="16px"
-              className="sidenav-links"
-              color="gray.600"
-              key={menu.name}
-            >
-              <LinkBox>
-                <NextLink
-                  href={menu.route}
-                  passHref
-                >
-                  <LinkOverlay>
-                    <ListIcon
-                      as={menu.icon}
-                      color="gray.500"
-                      marginRight="20px"
-                    />
-                    {menu.name}
-                  </LinkOverlay>
-                </NextLink>
-              </LinkBox>
-            </ListItem>
-          ))}
-        </List>
-        <Divider
-          className="side-divider"
-          color="gray.500"
-        />
-        <List spacing={2}>{playlists}</List>
+   return (
+      <Box className="side-wrapper">
+         <Box className="side_inner-wrapper">
+            <Box className="logo">
+               <Flex
+                  display="flex"
+                  justifyContent="flex-start"
+                  width="100%">
+                  <NextImage
+                     src={bart}
+                     height={38}
+                     width={30}
+                     alt="Bart"
+                  />{' '}
+                  <Text
+                     ml={2}
+                     textTransform="uppercase"
+                     fontSize="sm"
+                     marginLeft="0.5rem"
+                     lineHeight="260%"
+                     fontWeight="bold"
+                     color="red.500">
+                     TMX Media
+                  </Text>
+               </Flex>
+            </Box>
+            <List spacing={2}>
+               {navMenu().map(menu => (
+                  <ListItem
+                     className="sidenav-links"
+                     color="gray.600"
+                     paddingX="20px"
+                     fontSize="16px"
+                     key={menu.name}>
+                     <LinkBox>
+                        <NextLink
+                           href={menu.route}
+                           passHref>
+                           <LinkOverlay>
+                              <ListIcon
+                                 as={menu.icon}
+                                 color="gray.500"
+                                 marginRight="20px"
+                              />
+                              {menu.name}
+                           </LinkOverlay>
+                        </NextLink>
+                     </LinkBox>
+                  </ListItem>
+               ))}
+            </List>
+            <Divider
+               className="side-divider"
+               color="gray.500"
+            />
+            <List spacing={2}>
+               {musicMenu().map(menu => (
+                  <ListItem
+                     paddingX="20px"
+                     fontSize="16px"
+                     className="sidenav-links"
+                     color="gray.600"
+                     key={menu.name}>
+                     <LinkBox>
+                        <NextLink
+                           href={menu.route}
+                           passHref>
+                           <LinkOverlay>
+                              <ListIcon
+                                 as={menu.icon}
+                                 color="gray.500"
+                                 marginRight="20px"
+                              />
+                              {menu.name}
+                           </LinkOverlay>
+                        </NextLink>
+                     </LinkBox>
+                  </ListItem>
+               ))}
+            </List>
+            <Divider
+               className="side-divider"
+               color="gray.500"
+            />
+            <List spacing={2}>
+               {musicMenu().map(menu => (
+                  <ListItem
+                     paddingX="20px"
+                     fontSize="16px"
+                     className="sidenav-links"
+                     color="gray.600"
+                     key={menu.name}>
+                     <LinkBox>
+                        <NextLink
+                           href={menu.route}
+                           passHref>
+                           <LinkOverlay>
+                              <ListIcon
+                                 as={menu.icon}
+                                 color="gray.500"
+                                 marginRight="20px"
+                              />
+                              {menu.name}
+                           </LinkOverlay>
+                        </NextLink>
+                     </LinkBox>
+                  </ListItem>
+               ))}
+            </List>
+            <Divider
+               className="side-divider"
+               color="gray.500"
+            />
+            <List spacing={2}>{playlists}</List>
+         </Box>
       </Box>
-    </Box>
-  )
+   )
 }
 
 export default Sidebar
